ca3334c9cc1b3135f8f00c68cab7e3025db0fa31
PeterHaubenPXL <peter.hauben@student.pxl.be>
2024-10-21 14:21:03 +0200

    TextBox aangepast naar PasswordBox

--- Login-scherm/Login-scherm/Login-scherm/MainWindow.xaml.cs
+++ Login-scherm/Login-scherm/Login-scherm/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Login_scherm
 
         private void LogInButton_Click(object sender, RoutedEventArgs e)
         {
-            if (NameTextBox.Text.Equals("admin") && PasswordTextBox.Text.Equals("admin"))
+            if (NameTextBox.Text.Equals("admin") && PasswordTextBox.Password.Equals("admin"))
             {
                 InfoTextBlock.Text = "U wordt ingelogd.";
             }
